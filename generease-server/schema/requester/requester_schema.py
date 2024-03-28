462cb6a7266569d7ae1e351c2c5bffa84b5699b3
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-03-28 09:22:00 +0900

    Revert "first commit"
    This reverts commit 1de9e998a036f0ecc7e95d3379b28b3c4aeef9b5.

--- generease-server/schema/requester/requester_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class RequesterApply(BaseModel):
-    responser_id: int
-
-class ApplyResult(BaseModel):
-    requester_id: int
-    apply: str
-    
--- generease-server/util/error.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException
-from starlette import status
-
-credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="COULD NOT VALIDATE CREDENTIALS",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
